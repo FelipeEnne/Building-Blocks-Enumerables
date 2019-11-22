776268e065ce450492aa21b67bff48326e9eeb49
FelipeEnne <felipeenne@gmail.com>
2019-11-22 12:21:31 -0300

    create my_each_with_index

--- enumerables.rb
+++ enumerables.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 module Enumerable
+
   def my_each
     x = 0
     while x < self.size
@@ -8,6 +9,15 @@ module Enumerable
       x += 1
     end
   end
+
+  def my_each_with_index
+    x = 0
+    while x < self.size
+      yield(self[x], x)
+      x += 1
+    end
+  end
+
 end
 
 # test
@@ -16,7 +26,7 @@ array1 = [1, 2, 3, 4, 5]
 array2 = [1, 1, 2, 2, 2]
 testproc = Proc.new { |x| x * x }
 
-#=begin
+=begin
 # my_each
 print "my_each output\: "
 array1.my_each { |item| print item }
@@ -24,4 +34,11 @@ puts " "
 print "each output\: "
 array1.each { |item| print item }
 puts " "
-#=end
+=end
+# my_each_with_index
+print "my_each_with_index output\: "
+array1.my_each_with_index { |item| print item }
+puts " "
+print "each_with_index output\: "
+array1.each_with_index { |item| print item }
+puts " "
