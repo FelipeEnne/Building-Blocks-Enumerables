7cf5f42b561cc8e1eb5d0825878aa655af8d6ca5
FelipeEnne <felipeenne@gmail.com>
2019-11-22 13:11:33 -0300

    create a test file, my_map and my_inject

--- enumerables.rb
+++ enumerables.rb
@@ -21,29 +21,57 @@ module Enumerable
 
   def my_select
     array = []
-    self.my_each { |x| array << x if yield(x) }
-    return array
+    selfitem = self
+    selfitem.my_each { |x| array << x if yield(x) }
+    array
   end
 
   def my_all?
-    self.my_each { |x| return false if !yield(x) }
+    selfitem = self
+    selfitem.my_each { |x| return false unless yield(x) }
     return true
   end
 
   def my_any?
-    self.my_each { |x| return true if yield(x) }
+    selfitem = self
+    selfitem.my_each { |x| return true if yield(x) }
     return false
   end
 
   def my_none?
-    self.my_each { |x| return false if yield(x) }
+    selfitem = self
+    selfitem.my_each { |x| return false if yield(x) }
     return true
   end
 
   def my_count
     array = []
-    self.my_each { |x| array << x if yield(x) }
+    selfitem = self
+    selfitem.my_each { |x| array << x if yield(x) }
     return array.length
+  end
+
+  def my_map(proc = nil)
+    array = []
+    selfitem = self
+    if proc == true
+      selfitem.my_each { |x| array << proc.call(x) }
+    else
+      selfitem.my_each { |x| array << yield(x) }
+    end
+    return array
+  end
+
+  def my_inject(s = 0)
+    array = []
+    sum = s
+    selfitem = self
+    selfitem.my_each { |x| sum = yield(sum, x) }
+    return sum
+  end
+
+  def multiply_els(array)
+    array.my_inject(1) { |x, y| x * y }
   end
 end
 
@@ -51,84 +79,30 @@ end
 include Enumerable
 array1 = [1, 2, 3, 4, 5]
 array2 = [1, 1, 2, 2, 2]
-testproc = Proc.new { |x| x * x }
+testproc = proc { |x| x * x }
 
-=begin
-# my_each
-print "my_each output\: "
-array1.my_each { |item| print item }
-puts " "
-print "each output\: "
-array1.each { |item| print item }
-puts " "
-
-# my_each_with_index
-print "my_each_with_index output\: "
-array1.my_each_with_index { |item| print item }
-puts " "
-print "each_with_index output\: "
-array1.each_with_index { |item| print item }
-puts " "
-
-# my_select
-print "my_select output\: "
-print array1.my_select { |item| item > 2 }
-puts " "
-print "select output\: "
-print array1.select { |item| item > 2 }
-puts " "
-
-# my_all?
-print "my_all? output\: "
-print array1.my_all? {|number| number > 5}
+# my_map
+print "my_map output\:"
+print array1.my_map { |x| x * x }
 print " / "
-print array1.my_all? {|number| number <= 5}
+print array1.my_map(&testproc)
 puts " "
 
-print "all? output\: "
-print array1.all? {|number| number > 5}
+print "map output\:"
+print array1.map { |x| x * x }
 print " / "
-print array1.all? {|number| number <= 5}
+print array1.map(&testproc)
 puts " "
 
-# my_any?
-print "my_any? output\: "
-print array1.my_any? {|number| number > 5}
-print " / "
-print array1.my_any? {|number| number >= 5}
+# my_inject
+print "my_inject output\: "
+print array1.my_inject { |sum, n| sum + n }
 puts " "
 
-print "any? output\: "
-print array1.any? {|number| number > 5}
-print " / "
-print array1.any? {|number| number >= 5}
+print "inject output\: "
+print array1.inject { |sum, n| sum + n }
 puts " "
 
-
-# my_none?
-print "my_none? output\: "
-print array1.my_none? {|number| number > 5}
-print " / "
-print array1.my_none? {|number| number >= 5}
+print "multiply_els output\:"
+print multiply_els([2, 4, 5])
 puts " "
-
-print "none? output\: "
-print array1.none? {|number| number > 5}
-print " / "
-print array1.none? {|number| number >= 5}
-puts " "
-
-
-# my_count
-print "my_count output\: "
-print array1.my_count {|number| number == 2}
-print " / "
-print array2.my_count {|number| number == 2}
-puts " "
-
-print "count output\: "
-print array1.count {|number| number == 2}
-print " / "
-print array2.count {|number| number == 2}
-puts " "
-=end
